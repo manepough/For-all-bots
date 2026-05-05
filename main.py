a97ae26e7cd9246350ffeddc6c6978994732c50f
manepough <manepough@gmail.com>
2026-05-05 18:12:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,5 @@ def notify():
     return jsonify({"ok": True}), 200
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 10000)) 
     app.run(host="0.0.0.0", port=port)
-
-bot.run(DISCORD_BOT_TOKEN)
